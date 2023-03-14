1352a5bc2eb059770cf5043db83f15116330c1f1
Lucas Tostes <lucas.tostes@wildlifestudios.com>
2023-03-14 19:57:38 -0300

    moved react components to inside fun

--- src/visualizations/my-custom-viz.tsx
+++ src/visualizations/my-custom-viz.tsx
@@ -11,25 +11,6 @@ interface WhateverNameYouWantVisualization extends VisualizationDefinition {
     elementRef?: HTMLDivElement,
 }
 
-const rows: GridRowsProp = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-  ];
-  
-const columns: GridColDef[] = [
-{ field: 'col1', headerName: 'Column 1', width: 150 },
-{ field: 'col2', headerName: 'Column 2', width: 150 },
-];
-
-function TestTable() {
-return (
-    <div style={{ height: 300, width: '100%' }}>
-    <DataGrid rows={rows} columns={columns} />
-    </div>
-);
-}
-
 const vis: WhateverNameYouWantVisualization = {
     id: 'some id', // id/label not required, but nice for testing and keeping manifests in sync
     label: 'Some Name',
@@ -63,15 +44,22 @@ const vis: WhateverNameYouWantVisualization = {
         });
         if (errors) { // errors === true means no errors
 
-            
-
-
-            // element.innerHTML = 'Hello MR HAAAANNNN!';
-
+            const rows: GridRowsProp = [
+                { id: 1, col1: 'Hello', col2: 'World' },
+                { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+                { id: 3, col1: 'MUI', col2: 'is Amazing' },
+              ];
+              
+            const columns: GridColDef[] = [
+            { field: 'col1', headerName: 'Column 1', width: 150 },
+            { field: 'col2', headerName: 'Column 33', width: 150 },
+            ];
 
             ReactDOM.render(
                 <React.StrictMode>
-                    <TestTable/>
+                    <div style={{ height: 300, width: '100%' }}>
+                        <DataGrid rows={rows} columns={columns} />
+                    </div>
                 </React.StrictMode>,
                 element
             );
